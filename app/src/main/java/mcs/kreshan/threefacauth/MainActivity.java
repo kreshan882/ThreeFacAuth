04816d578f7e380fd1b3bee348fbfbafdd43133e
kreshan882@gmail.com <kreshan882@gmail.com>
2017-08-24 09:52:20 +0530

    ok

--- app/src/main/java/mcs/kreshan/threefacauth/MainActivity.java
+++ app/src/main/java/mcs/kreshan/threefacauth/MainActivity.java
@@ -14,4 +14,44 @@ public class MainActivity extends AppCompatActivity {
         Log.i(LOG_CLASS,"onCreate");
 
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_CLASS, "On Destroy .....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_CLASS, "On Pause .....");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_CLASS, "On Restart .....");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_CLASS, "On Resume .....");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_CLASS, "On Start .....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_CLASS, "On Stop .....");
+    }
+
 }
