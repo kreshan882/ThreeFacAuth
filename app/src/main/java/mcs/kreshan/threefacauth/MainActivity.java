e1d49e4f7b12e117eb93e000e078a9a35edfefab
kreshan882@gmail.com <kreshan882@gmail.com>
2017-08-24 09:03:32 +0530

    ok

--- app/src/main/java/mcs/kreshan/threefacauth/MainActivity.java
+++ app/src/main/java/mcs/kreshan/threefacauth/MainActivity.java
@@ -3,12 +3,15 @@ package mcs.kreshan.threefacauth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String LOG_CLASS="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LOG_CLASS,"onCreate");
 
     }
 }
