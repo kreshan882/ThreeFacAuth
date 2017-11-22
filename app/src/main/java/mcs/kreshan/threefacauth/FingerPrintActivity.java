c1c37cfe4cc08331895bd2df296762ab4b573df1
kreshan882@gmail.com <kreshan882@gmail.com>
2017-11-22 07:28:01 +0530

    ok

--- app/src/main/java/mcs/kreshan/threefacauth/FingerPrintActivity.java
+++ app/src/main/java/mcs/kreshan/threefacauth/FingerPrintActivity.java
@@ -17,6 +17,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
+import java.security.KeyStore;
+
+import javax.crypto.Cipher;
+
 import mcs.kreshan.fingerprint.FingerPrintAuthImplementation;
 import mcs.kreshan.fingerprint.FingerPrintAuthInterface;
 import mcs.kreshan.fingerprint.FingerPrintUtils;
@@ -28,14 +32,15 @@ import mcs.kreshan.utill.ResponceCode;
 
 public class FingerPrintActivity extends AppCompatActivity implements FingerPrintAuthInterface {
     public static final String LOG_CLASS = "MainActivity";
-    private TextView mAuthMsgTv;
-    private ViewSwitcher mSwitcher;
-    private Button mGoToSettingsBtn;
-    private FingerPrintAuthImplementation mFingerPrintAuthHelper;
+//    private TextView mAuthMsgTv;
+//    private ViewSwitcher mSwitcher;
+//    private Button mGoToSettingsBtn;
+//    private FingerPrintAuthImplementation mFingerPrintAuthHelper;
 
-//    private KeyStore keyStore;
-//    private static final String KEY_NAME = "androidHive";
-//    private Cipher cipher;
+    private KeyStore keyStore;
+    private static final String KEY_NAME = "androidHive";
+    private Cipher cipher;
+    private TextView auth_message_tv;
 
     @TargetApi(Build.VERSION_CODES.M)
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -45,41 +50,8 @@ public class FingerPrintActivity extends AppCompatActivity implements FingerPrin
         setContentView(R.layout.finger_print);
 
         Log.i(LOG_CLASS,"FingerPrintActivity.onCreate");
-        mGoToSettingsBtn = (Button) findViewById(R.id.go_to_settings_btn); //button setting
-        mGoToSettingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(LOG_CLASS,"FingerPrintActivity.onCreate.setOnClickListener");
-                FingerPrintUtils.openSecuritySettings(FingerPrintActivity.this);
-            }
-        });
 
-        mSwitcher = (ViewSwitcher) findViewById(R.id.main_switcher); //viewSwitcher
-        mAuthMsgTv = (TextView) findViewById(R.id.auth_message_tv); //textview msg
 
-        EditText pinEt = (EditText) findViewById(R.id.pin_et); //enter pin
-        pinEt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.i(LOG_CLASS,">>>>>>>>>>>>>>>>>>>>>>>>>"+s.toString());
-                if (s.toString().equals("1234")){
-                    Toast.makeText(FingerPrintActivity.this, "Authentication succeeded.", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(FingerPrintActivity.this, FingerPrintSuccessActivity.class));
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        Log.i(LOG_CLASS,"FingerPrintActivity.onCreat getHelper start");
-        mFingerPrintAuthHelper = FingerPrintAuthImplementation.getHelper(this, this);
         Log.i(LOG_CLASS,"FingerPrintActivity.onCreat getHelper end");
 
 
@@ -89,40 +61,32 @@ public class FingerPrintActivity extends AppCompatActivity implements FingerPrin
     protected void onResume() {
         Log.i(LOG_CLASS,"FingerPrintActivity.onResume");
         super.onResume();
-        mGoToSettingsBtn.setVisibility(View.GONE);
 
-        mAuthMsgTv.setText("Scan your finger");
-
-        //start finger print authentication
-        mFingerPrintAuthHelper.startAuth();
     }
 
     @Override
     protected void onPause() {
         Log.i(LOG_CLASS,"FingerPrintActivity.onPause");
         super.onPause();
-        mFingerPrintAuthHelper.stopAuth();
+
     }
 
     @Override
     public void onNoFingerPrintHardwareFound() {
         Log.i(LOG_CLASS,"FingerPrintActivity.onNoFingerPrintHardwareFound");
-        mAuthMsgTv.setText("Your device does not have finger print scanner. Please type 1234 to authenticate.");
-        mSwitcher.showNext();
+
     }
 
     @Override
     public void onNoFingerPrintRegistered() {
         Log.i(LOG_CLASS,"FingerPrintActivity.onNoFingerPrintRegistered");
-        mAuthMsgTv.setText("There are no finger prints registered on this device. Please register your finger from settings.");
-        mGoToSettingsBtn.setVisibility(View.VISIBLE);
+
     }
 
     @Override
     public void onBelowMarshmallow() {
         Log.i(LOG_CLASS,"FingerPrintActivity.onBelowMarshmallow");
-        mAuthMsgTv.setText("You are running older version of android that does not support finger print authentication. Please type 1234 to authenticate.");
-        mSwitcher.showNext();
+
     }
 
     @Override
@@ -136,17 +100,6 @@ public class FingerPrintActivity extends AppCompatActivity implements FingerPrin
     public void onAuthFailed(int errorCode, String errorMessage) {
         Log.i(LOG_CLASS,"FingerPrintActivity.onAuthFailed");
         Log.i(LOG_CLASS,"onAuthFailed"+errorCode);
-        switch (errorCode) {
-            case ResponceCode.CANNOT_RECOGNIZE_ERROR:
-                mAuthMsgTv.setText("Cannot recognize your finger print. Please try again.");
-                break;
-            case ResponceCode.NON_RECOVERABLE_ERROR:
-                mAuthMsgTv.setText("Cannot initialize finger print authentication. Please type 1234 to authenticate.");
-                mSwitcher.showNext();
-                break;
-            case ResponceCode.RECOVERABLE_ERROR:
-                mAuthMsgTv.setText(errorMessage);
-                break;
-        }
+
     }
 }
