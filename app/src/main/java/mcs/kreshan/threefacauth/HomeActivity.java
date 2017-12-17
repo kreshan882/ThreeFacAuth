0ad00173b6954f6b9aab68a122b899ad334ba046
kreshan882@gmail.com <kreshan882@gmail.com>
2017-12-18 00:49:09 +0530

    ok

--- app/src/main/java/mcs/kreshan/threefacauth/HomeActivity.java
+++ app/src/main/java/mcs/kreshan/threefacauth/HomeActivity.java
@@ -1,18 +1,36 @@
 package mcs.kreshan.threefacauth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by kreshan88 on 12/17/2017.
  */
 
 public class HomeActivity extends AppCompatActivity {
+    public static final String LOG_CLASS = "MainActivity";
+    Button but_qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        but_qr=(Button)findViewById(R.id.read_qrcord_but);
+        but_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(LOG_CLASS,"home function read QR cord");
+
+                Intent intent=new Intent(HomeActivity.this, QRCordActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/mcs/kreshan/threefacauth/QRCordActivity.java
@@ -0,0 +1,154 @@
+package mcs.kreshan.threefacauth;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+/**
+ * Created by kreshan88 on 12/18/2017.
+ */
+
+public class QRCordActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    public static final String LOG_CLASS="MainActivity";
+    private static final int REQUEST_CAMERA=1;
+    private ZXingScannerView scannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView=new ZXingScannerView(this);
+        setContentView(scannerView);
+        // setContentView(R.layout.activity_main);
+        Log.i(LOG_CLASS,"onCreate1");
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Log.i(LOG_CLASS,"onCreate2");
+            if(checkPermition()){
+                Toast.makeText(this,"permition have",Toast.LENGTH_LONG).show();
+                Log.i(LOG_CLASS,"onCreate3");
+            }else{
+                requestPermissions();
+                Log.i(LOG_CLASS,"onCreate4");
+            }
+
+        }
+    }
+
+    private boolean checkPermition(){
+        return (ContextCompat.checkSelfPermission(QRCordActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermissions(){
+        ActivityCompat.requestPermissions(QRCordActivity.this,
+                new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+    }
+
+    public void onRequestPermissionResult(int requestCord,String permission[],int grandResult[]) {
+        Log.i(LOG_CLASS, "onRequestPermissionResult");
+        switch (requestCord) {
+            case REQUEST_CAMERA:
+                if (grandResult.length > 0) {
+
+                    boolean cameraAccess = grandResult[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccess) {
+                        Toast.makeText(QRCordActivity.this, "permition granded", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(QRCordActivity.this, "permition denide", Toast.LENGTH_LONG).show();
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                            displayAllertMessage("do u need allow confirmantion",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                                        }
+                                    }
+                            );
+                            return;
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    public void displayAllertMessage(String message, DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(QRCordActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("cancle",null)
+                .create()
+                .show();
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(LOG_CLASS,"onResume");
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Log.i(LOG_CLASS,"version ok");
+            if(checkPermition()){
+                Log.i(LOG_CLASS,"permition ok");
+                Toast.makeText(QRCordActivity.this,"permition have",Toast.LENGTH_LONG).show();
+                if(scannerView==null){
+                    Log.i(LOG_CLASS,"scannerView");
+                    scannerView=new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+                Log.i(LOG_CLASS,"startCamera");
+            }else{
+                Log.i(LOG_CLASS,"requestPermition");
+                requestPermissions();
+            }
+
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        scannerView.stopCamera();
+
+    }
+    @Override
+    public void handleResult(Result result) {
+        final String resultmy = result.getText();
+        Log.i(LOG_CLASS,">>>>>>:"+resultmy);
+
+//        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+//        builder.setTitle("scan result");
+//        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                scannerView.resumeCameraPreview(MainActivity.this);
+//            }
+//        });
+//        builder.setNegativeButton("cancle", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Intent interent=new Intent(Intent.ACTION_VIEW, Uri.parse(resultmy));//go to that url
+//                startActivity(interent);
+//            }
+//        });
+//        builder.setMessage(resultmy);
+//        AlertDialog alert=builder.create();
+//        alert.show();
+
+
+    }
+
+
+}
+
